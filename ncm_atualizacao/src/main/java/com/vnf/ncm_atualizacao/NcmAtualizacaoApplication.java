669fae8f298cf33418901471966b76af0f5ebfa7
Vinicius Nascimento <50060629+Vnf13@users.noreply.github.com>
2025-08-29 17:32:01 -0300

    Add files via upload
    Inicio do projeto de criação do gerador de tabela NCM (IBPT) atualizado

---
+++ ncm_atualizacao/src/main/java/com/vnf/ncm_atualizacao/NcmAtualizacaoApplication.java
@@ -0,0 +1,13 @@
+package com.vnf.ncm_atualizacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcmAtualizacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcmAtualizacaoApplication.class, args);
+	}
+
+}
---
+++ ncm_atualizacao/src/test/java/com/vnf/ncm_atualizacao/NcmAtualizacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vnf.ncm_atualizacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcmAtualizacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
